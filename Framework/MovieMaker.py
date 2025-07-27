057e1e1484ea393862c430e5b30a23c82fe25a36
Christoph Lürig <christoph.luerig@gmail.com>
2025-07-27 07:03:42 +0200

    MovieMaker compatible with up to date moviepy

--- Framework/MovieMaker.py
+++ Framework/MovieMaker.py
@@ -6,13 +6,10 @@ Created on Fri Jul 25 12:58:52 2025
 """
 
 
-# pip uninstall moviepy
-# pip install moviepy==1.0.3
-
 import pygame as pg
 import Framework.ScriptGenerator as ScriptGenerator
 
-import moviepy.editor as mp
+import moviepy.video.VideoClip as Video
 
 class MovieMaker:
     def __init__(self, painterGenerator):
@@ -22,28 +19,26 @@ class MovieMaker:
         
         
     def __makeFrame(self, time):
-        """Generate a frame at time t (in seconds)"""
-       
-        simulationTime = time * self.__timeScale
         self.__painter.DrawStaticParts(self.__drawingSurface)
-        
-        # Ask our script.
-        situation = self.__script.GetAllInterpolatedEntries(simulationTime)
-        self.__painter.DrawElements(self.__drawingSurface, situation)
-        
-            
+        self.__painter.DrawElements(self.__drawingSurface,
+                                    self.__script.GetAllInterpolatedEntries(time))
         self.__painter.DrawFinalOverlay(self.__drawingSurface)  
         # Convert to numpy array
         array = pg.surfarray.array3d(self.__drawingSurface)
         array = array.swapaxes(0, 1)
         return array     
+    
+    
+   
+        
         
     def MakeMovie(self, movieFilename, scriptFilename, fps, timeScale):
         self.__script = ScriptGenerator.ScriptGenerator.LoadScript(scriptFilename+'.pkl')
         self.__timeScale = timeScale
-        movieTime = self.__script.MaxTime / timeScale
         self.__drawingSurface = pg.Surface(self.__painter.ImageSize)
-        clip = mp.VideoClip(self.__makeFrame, duration=movieTime)
-        clip.fps = fps
+        deltaTime = timeScale / fps
+        amounOfFrames = int(self.__script.MaxTime / deltaTime)
+        dataList = [frame * deltaTime for frame in range(amounOfFrames)]
+        clip = Video.DataVideoClip(dataList, self.__makeFrame, fps)
         clip.write_videofile(movieFilename + ".mp4", codec='libx264')
         
