01132088e3256ac0d4df3fe5f4ef3c7f60e3d8c1
Christoph Lürig <christoph.luerig@gmail.com>
2025-07-27 21:36:57 +0200

    optimized training parameters

--- FactoryExample/TestFunctions.py
+++ FactoryExample/TestFunctions.py
@@ -11,7 +11,9 @@ import Framework.GlobalFunctions as Global
 import FactoryExample.FactoryPainter as Painter
 
 
-Global.PerformTraining("TestModel", FactoryPlugin.FactoryPlugin,sizeOfMacroBatch=20_000, evaluationRuns=1, macroBatches=30, numOfParallelEnvs = 2)
+Global.PerformTraining("TestModel", FactoryPlugin.FactoryPlugin,sizeOfMacroBatch=20_000, 
+                       evaluationRuns=1, macroBatches=30, numOfParallelEnvs = 2,
+                       additionalPPOargs = {'gamma' : 1.0, 'ent_coef' : 0.05})
 Global.GenerateMovie("TestMovie", "TestModel", FactoryPlugin.FactoryPlugin,  Painter.FactoryPainter, 30, 5.0 )
 
 
